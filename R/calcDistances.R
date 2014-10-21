482e950d47edc71c0ff9c5239d58cda73a6c4509
fischuu <github@danielfischer.name>
2014-10-21 21:15:01 +0300

    Updated manual and calcDistances, ongoing...

--- R/calcDistances.R
+++ R/calcDistances.R
@@ -1,19 +1,23 @@
 # Experimental parts on time series clustering and the corresponding distances measures
 
-calcDistances <- function(X,method="C",nodes=20){
+calcDistances <- function(X,method=c("euclidean","splines"),df=NULL){
+  method <- match.arg(method)
   res <- c()
-  if(method=="C") res <- calcDistances.C(X)
+  if(method=="euclidean") res <- calcDistances.C(X)
   if(method=="splines") res <- calcDistances.Splines(X,nodes=nodes)
   res
 } 
 
-calcDistances.Splines <- function(X,nodes){
+calcDistances.Splines <- function(X,df){
   NC <- ncol(X)
   NR <- nrow(X) 
-  for(i in 1:NC)
+  for(i in 1:NR)
   {
-    temp <- smooth.spline(1:NC, X[i,], df=NC, all.knots=TRUE)
+    temp[[i]] <- smooth.spline(1:NC, X[i,], df=df, all.knots=TRUE)
     deriv0 <- predict(temp,seq(1,NC,0.1),deriv=0)$y
-    deriv1 <- predict(temp,seq(1,NC,0.1),deriv=1)$y
+    #deriv1 <- predict(temp,seq(1,NC,0.1),deriv=1)$y
   }
 }
+
+X <- matrix(sample(1000,200),ncol=20)
+calcDistances(X, method="sp", df=18)
--- src/timeCluster.cpp
+++ src/timeCluster.cpp
@@ -15,14 +15,14 @@ RcppExport SEXP calcDistances(SEXP X){
     distances.zeros();
      for(int i=0;i<p;i++){
        for(int j=i;j<p;j++){
-	 for(int k=0;k<q;k++){
-	  distances(i,j) = distances(i,j) + (XXX(i,k)-XXX(j,k))*(XXX(i,k)-XXX(j,k)); 
-	 }
+      	 for(int k=0;k<q;k++){
+	          distances(i,j) = distances(i,j) + (XXX(i,k)-XXX(j,k))*(XXX(i,k)-XXX(j,k)); 
+	       }
        }
      }
  
     List res;
-    res["distMat"] = distances;
+    res["distMat"] = sqrt(distances);
     
     return res;
 }
