94bfee8eefacef08b71662ec1aa072c9409ea333
Daniel Fischer <github@danielfischer.name>
2015-10-23 17:26:03 +0300

    Remove cran check notes for the description file

---
+++ R/annotTrack.R
@@ -0,0 +1,5 @@
+annotTrack <- function()
+{
+  .Deprecated(msg="The example data will be replaced by a more real data. Download it via ...")
+  ## use new function, or remainder of myOldFunc
+}
--- R/calcDistances.R
+++ R/calcDistances.R
@@ -1,12 +1,13 @@
 # Experimental parts on time series clustering and the corresponding distance measures
 
-calcDistances <- function(X,method="euclidean", spar=0.1, ...){
-  method <- match.arg(method, c("euclidean", "splines"))
+calcDistances <- function(X,method="euclidean", ...){
+  method <- match.arg(method, c("euclidean"))
   res <- c()
   if(method=="euclidean") res <- calcDistances.C(X)$distMat
-  if(method=="splines") res <- calcDistances.Splines(X, spar, ...)
   res
 } 
+
+# Section with new subfunction implementations
 
 calcDistances.Splines <- function(X, spar, ...){
   NC <- ncol(X)
@@ -21,14 +22,14 @@ calcDistances.Splines <- function(X, spar, ...){
   #  spline2 <- splinefun(1:100,X[2,], method="natural")
     
   #  plot(1:100,X[1,])
-    curve(spline2(x), from=1, to=10, col=1, n=1001)
-    curve(spline1(x), add=TRUE, col=2, n=1001)
-    curve(spline2(x), add=TRUE, col=3, n=1001)
-    abline(0,0)
-    points(X[1,])
-    points(X[2,])
-    
-    integrate(temp[[1]], lower=1, upper=100, subdivisions=1000)$value
-  }
-  temp
+#    curve(spline2(x), from=1, to=10, col=1, n=1001)
+#    curve(spline1(x), add=TRUE, col=2, n=1001)
+#    curve(spline2(x), add=TRUE, col=3, n=1001)
+#    abline(0,0)
+#    points(X[1,])
+#    points(X[2,])
+#    
+#    integrate(temp[[1]], lower=1, upper=100, subdivisions=1000)$value
+#  }
+#  temp
 }
